9ed6d5b5e32d6c21b5d9e99185f5dcda5d8c41d2
AleixLinares <aleixlinaresbaldrich@gmail.com>
2026-05-09 15:26:25 +0200

    fix dropdowns and new number selector

---
+++ wwwroot/js/dropdown.js
@@ -0,0 +1,13 @@
+﻿window.dropdownInterop = {
+    registerOutsideClick: function (element, dotnetHelper) {
+
+        document.addEventListener('click', function (event) {
+
+            if (!element.contains(event.target)) {
+                dotnetHelper.invokeMethodAsync('CloseDropdown');
+            }
+
+        });
+
+    }
+};
